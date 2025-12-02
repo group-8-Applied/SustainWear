991c0c38e1386d794a1378fac12322e669eacdf4
AlfieSHU <182757675+AlfieSHU@users.noreply.github.com>
2025-12-02 17:24:28 +0000

    auto lowercase email when logging in

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	$login_msg = "";
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$email = $_POST["email"];
+		$email = strtolower(trim($_POST["email"]));
 		$password = $_POST["password"];
 
 		$db = new SQLite3("database.db");
